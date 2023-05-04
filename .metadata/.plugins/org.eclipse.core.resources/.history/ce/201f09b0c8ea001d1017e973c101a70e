1ea47186c33da6d6058c91adfd7aefae16556de7
Aditya <aditya.subramanian97@gmail.com>
2023-05-04 19:56:02 -0400

    Peripheral Structural Definition Included

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ce/201f09b0c8ea001d1017e973c101a70e
@@ -0,0 +1,14 @@
+/*
+ * Stm32f44xx_gpio.h
+ *
+ *  Created on: May 4, 2023
+ *      Author: User
+ */
+
+#ifndef INC_STM32F44XX_GPIO_H_
+#define INC_STM32F44XX_GPIO_H_
+
+#include "stm32f446xx.h"
+;;cdsf
+
+#endif /* INC_STM32F44XX_GPIO_H_ */
--- driver_stm32f446xx/Debug/Src/main.d
+++ driver_stm32f446xx/Debug/Src/main.d
@@ -1,3 +1,6 @@
-Src/main.o: ../Src/main.c ../Inc/stm32f446xx.h
+Src/main.o: ../Src/main.c ../Inc/stm32f446xx.h \
+ C:/Ad_Docs/Embedded\ Systems/git/driver_stm32f44/stm32_driver_development/driver_stm32f446xx/Driver/Inc/Stm32f44xx_gpio.h
 
 ../Inc/stm32f446xx.h:
+
+C:/Ad_Docs/Embedded\ Systems/git/driver_stm32f44/stm32_driver_development/driver_stm32f446xx/Driver/Inc/Stm32f44xx_gpio.h:
